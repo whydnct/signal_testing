bb95972f4dc466144b10764d938a3a38fb845264
aperez24 <aperez24@tragsa.es>
2023-09-05 22:56:00 +0200

    .

--- src/main.c
+++ src/main.c
@@ -6,7 +6,8 @@
 
 void child_sigint_handler(int signum) {
 	(void)signum;
-    printf("Child process received SIGINT signal. Exiting...\n");
+    printf("Child process received SIGINT signal.\n");
+    printf("Wanna kill it? Uncomment exit(0)...\n");
     //exit(0);
 }
 
@@ -16,28 +17,23 @@ int main() {
     // Create a new process
     child_pid = fork();
 
-    if (child_pid < 0) {
-        perror("Fork failed");
-        exit(1);
-    } else if (child_pid == 0) {
-        // This code is executed by the child process
-		signal(SIGINT, child_sigint_handler);
-        while (1) {
-			pause();
-            // Infinite loop
-            // Add your code here to define what the child process does
-        }
-    } else {
-		while(1)
-		{
-			signal(SIGINT, SIG_IGN);
-        	// This code is executed by the parent process
-        	printf("Child process with PID %d is running.\n", child_pid);
-        	// Parent process can continue to do other tasks
-			wait(NULL);
-			printf("Child died\n");
-			pause();
+	signal(SIGINT, SIG_IGN);
+	while(1)
+	{
+  		if (child_pid == 0) {
+  		    // This code is executed by the child process
+			signal(SIGINT, child_sigint_handler);
+  		    while (1) {
+				pause();
+  		        // Infinite loop
+			}
 		}
+      	// This code is executed by the parent process
+      	printf("Child process with PID %d is running.\n", child_pid);
+      	// Parent process can continue to do other tasks
+		wait(NULL);
+		printf("Child died\n");
+		pause();
     }
 
     return 0;
